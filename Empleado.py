4990bfaf83edf8583491c6f424a9889304c06817
Alfredo Bollati <bollatialfredo@gmail.com>
2017-01-13 00:52:42 -0300

    agregado metodo fullname

--- Empleado.py
+++ Empleado.py
@@ -1,13 +1,18 @@
 class Empleado:
 
-    def __init__(corcho, first, last, pay):
-        corcho.first = first
-        corcho.last = last
-        corcho.pay = pay
-        corcho.email = first + '.' + last + '@company.com'
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
 
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
 
 emp_1 = Empleado('David', 'Martin', 16400)
 emp_2 = Empleado('Alfredo', 'Bollati', 13500)
 
 print("en mano: " +str( emp_1.pay))
+
+print(emp_1.fullname())
+print(Empleado.fullname(emp_2))
